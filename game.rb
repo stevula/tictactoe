ae93f11e198b734bc1d96abc00098df26bc9ed4a
stevula <brodericksteven@gmail.com>
2016-03-09 11:43:57 -0800

    Add Game#run and #declare_status

--- game.rb
+++ game.rb
@@ -1,10 +1,11 @@
 class Game
-  attr_reader :player_1, :player_2, :winner
+  attr_reader :player_1, :player_2
 
   def initialize
     greet
     @player_1, @player_2 = initialize_players
-    @winner = nil
+    @board = Board.new
+    run
   end
 
   private
@@ -26,5 +27,34 @@ class Game
     puts "Player 2, you are #{player_2.glyph}."
 
     return player_1, player_2
+  end
+
+  def run
+    current_player = @player_1
+
+    while @board.status == :ongoing
+      puts @board
+      puts "Player #{current_player.number} (#{current_player.glyph}), enter row 0, 1, or 2:"
+      row_number = gets.to_i
+
+      puts "Player #{current_player.number} (#{current_player.glyph}), enter column 0, 1, or 2:"
+      column_number = gets.to_i
+
+      @board.mark(player: current_player, row: row_number, column: column_number)
+
+      # alternate player_number between 1 and 2 on each loop
+      current_player = current_player == @player_1 ? @player_2 : @player_1
+    end
+
+    puts @board
+
+    declare_status
+  end
+
+  def declare_status
+    victory = "Congratulations, Player #{@board.winner.number}!"
+    stalemate = "Stalemate!"
+
+    puts @board.status == :solved ? victory : stalemate
   end
 end
--- tictactoe.rb
+++ tictactoe.rb
@@ -1,33 +1,10 @@
+require_relative 'game'
+require_relative 'player'
 require_relative 'board'
 require_relative 'square'
-require_relative 'player'
 
-board = Board.new
+game = Game.new
 
-current_player = board.player_1
-while board.status == :ongoing
-  puts board
-  puts "Player #{current_player.number} (#{current_player.glyph}), enter row 0, 1, or 2:"
-  row_number = gets.to_i
-
-  puts "Player #{current_player.number} (#{current_player.glyph}), enter column 0, 1, or 2:"
-  column_number = gets.to_i
-
-  board.mark(player: current_player, row: row_number, column: column_number)
-
-  # alternate player_number between 1 and 2 on each loop
-  current_player = current_player == board.player_1 ? board.player_2 : board.player_1
-end
-
-puts board
-
-if board.status == :solved
- puts "Congratulations, Player #{board.winner.number}!"
-end
-
-if board.status == :stalemate
-  puts "Stalemate!"
-end
 
 # prevent player from picking an already marked space
 # prevent player from choosing the same glyph as another player
